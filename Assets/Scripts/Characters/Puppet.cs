80c2f3c09c6d5e7d3045a388fe365adf706558f7
Unknown <khamaz.ee@gmail.com>
2019-07-02 12:39:43 +0200

    Pathfinding and floaty done

--- Assets/Scripts/Characters/Puppet.cs
+++ Assets/Scripts/Characters/Puppet.cs
@@ -11,21 +11,27 @@
         [SerializeField]
         protected bool isPathing = false;
         protected Vector3 targetPos;
-
-        [SerializeField]
-        protected float deviationStrenght = 1.5f;
+        
         [SerializeField]
         protected float closeRangeValue = 0.3f;
+        //[SerializeField]
+        //protected float cornerBrake = 0.3f;
+
         [SerializeField]
-        protected float cornerBrake = 0.3f;
+        [Range(0.5f, 5f)]
+        protected float amplitude = 1f;
+        [SerializeField]
+        [Range(1f, 8f)]
+        protected float frequency = 0.3f;
 
         [SerializeField]
         [ReadOnly]
-        private Vector2 velocity;
+        private Vector2 currentVelocity;
 
+        /*
         [SerializeField]
         [Range(0, 0.5f)]
-        private float coefVelocity;
+        private float coefVelocity; */
 
         [SerializeField]
         [ReadOnly]
@@ -53,27 +59,10 @@
                 {
                     yield return null;
                     rb.velocity = GetGhostVelocityToward(previousPos, targetPos);
-                    velocity = rb.velocity;
-                    observedSpeed = velocity.magnitude;
                 }
                 previousPos = transform.position;
             }
             rb.velocity = Vector2.zero;
-            /*
-            for (int i = 0; i < path.Size; i++)
-            {
-                targetPos = path[i].CenterWorld;
-                rb.velocity = GetGhostVelocityToward(previousPos, targetPos);
-                while (!IsVeryCloseTo(path[i].CenterWorld))
-                {
-                    yield return null;
-                    rb.velocity = GetGhostVelocityToward(previousPos, targetPos);
-                    velocity = rb.velocity;
-                    observedSpeed = velocity.magnitude;
-                }
-            }
-            rb.velocity = Vector2.zero;
-            */
             isPathing = false;
         }
 
@@ -91,103 +80,35 @@
             float distanceFromPrevious = Vector3.Distance(transform.position, previousPos);
             float distanceTotal = Vector3.Distance(targetPos, previousPos);
 
+            float minDistance = Mathf.Min(distanceFromNext, distanceFromPrevious);
+            if (minDistance < 1f)
+            {
+                perpendicularCoeff = Mathf.Lerp(0.5f, 1f, Mathf.Clamp01(minDistance - closeRangeValue)); 
+            }
 
             float t = distanceFromNext / distanceTotal;
-
-            /*
-            if (distanceFromPrevious < 1 + closeRangeValue)
-            {
-                adjustedSpeed = Mathf.Lerp(speed, speed * cornerBrake, distanceFromPrevious - closeRangeValue);
-                perpendicularCoeff = Mathf.Lerp(1f, 0f, distanceFromPrevious - closeRangeValue);
-            }
-            else if (distanceFromNext < 1 + closeRangeValue)
-            {
-                adjustedSpeed = Mathf.Lerp(speed * cornerBrake, speed, distanceFromNext - closeRangeValue);
-                perpendicularCoeff = Mathf.Lerp(0f, 1f, distanceFromNext - closeRangeValue);
-            }*/
-
-            Vector3 remnantVelocity = rb.velocity * coefVelocity;
-            Vector3 direction = (remnantVelocity + (targetPos - transform.position).normalized).normalized * adjustedSpeed;
+            
+            Vector3 direction = (targetPos - transform.position).normalized * adjustedSpeed;
             Vector3 perpendicular = Vector3.Cross(direction, Vector3.forward).normalized;
 
 
-            return remnantVelocity + (direction + (perpendicular * (Mathf.Pow(Mathf.Cos(t),2f)-0.5f) * deviationStrenght * perpendicularCoeff));
+            Vector3 calculatedVelocity = direction + (perpendicular * Cosinus(Time.time, amplitude, frequency) *  perpendicularCoeff);
+
+
+            currentVelocity = calculatedVelocity;
+            observedSpeed = currentVelocity.magnitude;
+
+            return calculatedVelocity;
 
 
         }
-        /*
-        private Vector2 GetGhostVelocityToward(int index, TilePath path)
+        
+        
+        private float Cosinus(float t, float amplitude, float frequency)
         {
-            float adjustedSpeed = speed;
-            float perpendicularCoeff = 1f;
-
-            float distanceFromNext = Vector3.Distance(transform.position, targetPos);
-            float distanceFromPrevious = Vector3.Distance(transform.position, previousPos);
-            float distanceTotal = Vector3.Distance(targetPos, previousPos);
-
-
-            float t = distanceFromNext / distanceTotal;
-
-            /*
-            if (distanceFromPrevious < 1 + closeRangeValue)
-            {
-                adjustedSpeed = Mathf.Lerp(speed, speed * cornerBrake, distanceFromPrevious - closeRangeValue);
-                perpendicularCoeff = Mathf.Lerp(1f, 0f, distanceFromPrevious - closeRangeValue);
-            }
-            else if (distanceFromNext < 1 + closeRangeValue)
-            {
-                adjustedSpeed = Mathf.Lerp(speed * cornerBrake, speed, distanceFromNext - closeRangeValue);
-                perpendicularCoeff = Mathf.Lerp(0f, 1f, distanceFromNext - closeRangeValue);
-            }
-
-            Vector3 remnantVelocity = rb.velocity * coefVelocity;
-            Vector3 direction = (remnantVelocity + (targetPos - transform.position).normalized).normalized * adjustedSpeed;
-            Vector3 perpendicular = Vector3.Cross(direction, Vector3.forward).normalized;
-
-
-            return remnantVelocity + (direction + (perpendicular * (Mathf.Pow(Mathf.Cos(t), 2f) - 0.5f) * deviationStrenght * perpendicularCoeff));
-
-
-        }*/
-
-        private float GetVelocity(int index, TilePath path)
-        {
-            Func<float, Vector3> fun = GetDestinationFunction(index, path);
-
-            return 0f;
-
+            return amplitude * Mathf.Cos(t * frequency);
         }
-
-        private Func<float, Vector3> GetDestinationFunction(int index, TilePath path)
-        {
-            Vector3 start = path[index].CenterWorld;
-            if (index - path.Size < 3)
-            {
-                return (t) => Vector3.Lerp(start, path.Goal.CenterWorld, t);
-            }
-            else
-            {
-                return (t) => InterpolateQuadraticBezierCurve(
-                    path[index].CenterWorld, 
-                    path[index + 1].CenterWorld, 
-                    path[index + 2].CenterWorld, 
-                    t);
-            }
-        }
-
-        private Func<float, Vector3> GetBezierTurn(Vector3 turnCenter, Vector3 nextPoint)
-        {
-            Vector3 A = turnCenter - transform.position;
-            Vector3 C = (nextPoint - turnCenter).normalized * A.magnitude;
-
-            return (t) => InterpolateQuadraticBezierCurve(A, turnCenter, C, t);
-        }
-        private Vector3 InterpolateQuadraticBezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, float t)
-        {
-            return p0 * Mathf.Pow(1f - t, 2) + 
-                p1 * 2 * (1 - t) * t + 
-                p2 * Mathf.Pow(t, 2);
-        }
+        
 
         private void OnDrawGizmos()
         {
