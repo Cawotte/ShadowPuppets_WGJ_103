aa6db90392a4cec78183e8e5fe52391f0aa44de6
Unknown <khamaz.ee@gmail.com>
2019-07-03 00:03:40 +0200

    I MADE A RULER

---
+++ Assets/Scripts/Utils/RulerGameObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulerGameObject : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    private void OnDrawGizmos()
+    {
+        if (target != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, target.position);
+            UnityEditor.Handles.Label((transform.position + target.position) / 2,
+                Vector3.Distance(transform.position, target.position).ToString()
+                );
+        }
+    }
+}
