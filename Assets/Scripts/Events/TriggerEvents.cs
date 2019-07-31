0f4dd18e590b19663864ef524c14d92c82cfaaca
Unknown <khamaz.ee@gmail.com>
2019-07-31 13:01:32 +0200

    Revert "Deleted unused event scripts"
    This reverts commit 3600e3f0ad55e5c6c5149e7d75aa8b99b6927af5.

---
+++ Assets/Scripts/Events/TriggerEvents.cs
@@ -0,0 +1,75 @@
+﻿namespace WGJ.PuppetShadow
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System;
+    using UnityEngine.Events;
+    using System.Linq;
+
+    /// <summary>
+    /// Component to trigger events when colliding with certains marker triggerers (TriggererEvents) 
+    /// </summary>
+    public class TriggerEvents : MonoBehaviour
+    {
+        [SerializeField]
+        private List<TriggerType> isTriggeredBy = new List<TriggerType>();
+
+        public Action OnEnterTrigger = null;
+        public Action OnExitTrigger = null;
+
+        [SerializeField]
+        private UnityEvent OnEnterTriggerEvent = null;
+        [SerializeField]
+        private UnityEvent OnExitTriggerEvent = null;
+
+        private void Start()
+        {
+            if (GetComponent<Collider2D>() == null)
+            {
+                Debug.Log("No collider 2D detected with Trigger! Add one ! (" + gameObject.name + ")");
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (IsTriggerValid(collision))
+            {
+                OnEnterTriggerEvent?.Invoke();
+                OnEnterTrigger?.Invoke();
+            }
+        }
+
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+
+            if (IsTriggerValid(collision))
+            {
+                OnExitTrigger?.Invoke();
+                OnExitTriggerEvent?.Invoke();
+            }
+        }
+
+        private bool IsTriggerValid(Collider2D coll)
+        {
+            //check errors
+            if (coll == null || coll.gameObject == null) {
+                return false;
+            }
+
+            TriggererEvents triggerer = coll.GetComponent<TriggererEvents>();
+
+            //Check if has triggerer component
+            if (triggerer == null)
+            {
+                return false;
+            }
+
+            //Check if valid triggerer type
+            //If there's more than one common triggerer, it's valid.
+            return (triggerer.TriggererTypes.Intersect(isTriggeredBy).Count() > 0);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Events/TriggererEvents.cs
@@ -0,0 +1,17 @@
+﻿namespace WGJ.PuppetShadow
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Like a collision layer, but for trigger Interactables.
+    /// </summary>
+    public class TriggererEvents : MonoBehaviour
+    {
+        [SerializeField]
+        private List<TriggerType> triggererTypes;
+
+        public List<TriggerType> TriggererTypes { get => triggererTypes; }
+    }
+}
